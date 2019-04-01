8068c0e8b45c5467cb47a3271e2357b3f718d9aa
Bruno Selva <bruno.selva@integritas.net>
2019-04-01 12:29:57 -0500

    Versions updated and Graphite port added

--- influxdb/entrypoint.sh
+++ influxdb/entrypoint.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
 if [ ! -f "/var/lib/influxdb/.init" ]; then
-    exec influxd $@ &
+    exec influxd -config /etc/influxdb/influxdb.conf $@ &
 
     until wget -q "http://localhost:8086/ping" 2> /dev/null; do
         sleep 1
     done
 
     influx -host=localhost -port=8086 -execute="CREATE USER ${INFLUX_USER} WITH PASSWORD '${INFLUX_PASSWORD}' WITH ALL PRIVILEGES"
-    influx -host=localhost -port=8086 -execute="CREATE DATABASE ${INFLUX_DB}"    
-    
+    influx -host=localhost -port=8086 -execute="CREATE DATABASE ${INFLUX_DB}"
+
     touch "/var/lib/influxdb/.init"
 
     kill -s TERM %1
